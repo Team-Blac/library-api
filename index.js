eda897993d61a15765c8814706c7e9ca5a0d2dad
TreyAnizi <tracyaniziamoro1@gmail.com>
2025-03-10 15:50:14 +0000

    created express app

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from "express";
+import "dotenv/config";
+import mongoose from "mongoose";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=> {
-    console.log(`Server running on PORT ${PORT}`)
-})
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log ('Database connected'))
+.catch(err => console.log('error'))
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
