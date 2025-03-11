3d3a27931f9ad1fe44976b9f123a3cfa02c2f5c2
myoppong <michaeloppong03@gmail.com>
2025-03-11 05:46:49 +0000

    models commit

--- models/books.js
+++ models/books.js
@@ -1,1 +1,15 @@
-const greetings= "Hello"
+import { Schema, model } from "mongoose";
+
+const booksSchema =  new Schema({
+title: { type: String, required: true, unique:true},
+author: { type: String, required: true },
+genre: { type: String },
+description:{type: String},
+publishedYear: { type: Number },
+coverImage: { type: String },
+quantityAvailable:{type:Number, required:true, default:1},
+}, {
+    timestamps: true
+});
+
+export const booksModel = model('books',booksSchema);
